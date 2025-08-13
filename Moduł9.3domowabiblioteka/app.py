4346f97ff7415d48555fd58a0903418e74532646
Dawid <dawid.petkowicz123wp.pl@wp.pl>
2025-08-13 21:14:55 +0200

    Update app.py

--- Moduł9.3domowabiblioteka/app.py
+++ Moduł9.3domowabiblioteka/app.py
@@ -1,7 +1,7 @@
 
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField
+from wtforms import StringField, IntegerField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length, NumberRange
 import uuid
 import json
@@ -10,20 +10,17 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super_secret_key'
 
+BOOKS_FILE = 'books.json'
 
 class BookForm(FlaskForm):
     title = StringField('Tytuł', validators=[DataRequired(), Length(min=1, max=100)])
     author = StringField('Autor', validators=[DataRequired(), Length(min=1, max=100)])
     year = IntegerField('Rok wydania', validators=[DataRequired(), NumberRange(min=0, max=2100)])
     description = TextAreaField('Opis', validators=[Length(max=500)])
-
+    submit = SubmitField('Dodaj książkę')
 
 class DeleteForm(FlaskForm):
-    pass
-
-
-BOOKS_FILE = 'books.json'
-
+    submit = SubmitField('Usuń')
 
 def load_books():
     if not os.path.exists(BOOKS_FILE):
@@ -31,11 +28,9 @@ def load_books():
     with open(BOOKS_FILE, 'r', encoding='utf-8') as f:
         return json.load(f)
 
-
 def save_books(books):
     with open(BOOKS_FILE, 'w', encoding='utf-8') as f:
         json.dump(books, f, ensure_ascii=False, indent=2)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -57,16 +52,13 @@ def index():
 
     return render_template('index.html', books=books, form=form, delete_form=delete_form)
 
-
 @app.route('/delete/<book_id>', methods=['POST'])
 def delete_book(book_id):
-    form = DeleteForm()
-    if form.validate_on_submit():
-        books = load_books()
-        books = [b for b in books if b['id'] != book_id]
-        save_books(books)
+    book_id = int(book_id)  
+    books = load_books()
+    books = [b for b in books if b['id'] != book_id]
+    save_books(books)
     return redirect(url_for('index'))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
